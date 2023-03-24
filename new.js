9886db9032ec27cf578d4e8aaf827a75c43ee3fc
bhnghiaa <110017168+bhnghiaa@users.noreply.github.com>
2023-03-24 14:45:09 +0700

    Update and rename new to new.js

---
+++ new.js
@@ -0,0 +1,209 @@
+const arr = [ PVG_meals,  PVG_meals_1,  PVG_meals_2,  PVG_meals_3,  PVG_meals_4,
+              LVK,  LVK1,  LVK2,  LVK3,  LVK4,
+              VK,  VK1,  VK2,  VK3,  VK4,
+              PKL, PKL1, PKL2, PKL3, PKL4, ];
+
+const PVG_meals = {
+  img: "img/custom_meals/Grain-Free-Hot-Cereal.jpg",
+  categories: ["PALEO", " VEGAN", "GLUTEN-FREE"],
+  name: "Grain Free Hot Cereal",
+  description:
+    "This super easy n’oatmeal is made in just 3 minutes and it’s gluten-free, paleo, vegan",
+  Calories: 400,
+  NutriScore: 74,
+  Rating: 4.8,
+};
+const PVG_meals_1 = {
+  img: "img/custom_meals/Quinoa_Breakfast_Bowl.jpg",
+  categories: ["PALEO", " VEGAN", "GLUTEN-FREE"],
+  name: "Quinoa Breakfast Bowl",
+  description:
+    "Full of fruits, nuts, and with gluten free oats, this is not only nourishing but also delicious",
+  Calories: 390,
+  NutriScore: 75,
+  Rating: 4.5,
+};
+const PVG_meals_2 = {
+  img: "img/custom_meals/Cauliflower_oatmeal.jpg",
+  categories: ["PALEO", " VEGAN", "GLUTEN-FREE"],
+  name: "Cauliflower oatmeal",
+  description:
+    "Combo of rolled oats and cauliflower rice makes an easy way to sneak in some veggies, fiber, and micronutrients into a comforting bowl of oatmeal",
+  Calories: 410,
+  NutriScore: 96,
+  Rating: 4.8,
+};
+const PVG_meals_3 = {
+  img: "img/custom_meals/Avocado_salad.jpg",
+  categories: ["PALEO", " VEGAN", "GLUTEN-FREE"],
+  name: "Avocado salad",
+  description:
+    "Healthy green salad with fresh avocado, spinach, tomatoes and citrus lime dressing",
+  Calories: 400,
+  NutriScore: 92,
+  Rating: 4.8,
+};
+const PVG_meals_4 = {
+  img: "img/custom_meals/Fluffy_Sweet_Potato.jpg",
+  categories: ["PALEO", " VEGAN", "GLUTEN-FREE"],
+  name: "Fluffy Sweet Potato",
+  description:
+    "What do you eat when it’s too cold for a smoothie, pancakes sound too complicated, and you’ve had oats for breakfast for way too many days in a row? Fluffy Sweet Potato Breakfast Bowls, of course!",
+  Calories: 380,
+  NutriScore: 92,
+  Rating: 4.7,
+};
+const LVK = {
+  img: "img/custom_meals/Japanese_Gyozas.jpg",
+  categories: ["LACTOSE-Free", " VEGAN", "KETO"],
+  name: "Japanese Gyozas",
+  description:
+    "Juicy pork, cabbage, scallions, and shiitake mushrooms with a rich Ponzu sauce drizzle",
+  Calories: 420,
+  NutriScore: 74,
+  Rating: 4.6,
+};
+const LVK1 = {
+  img: "img/custom_meals/Classic_bacon_and_eggs.jpg",
+  categories: ["LACTOSE-Free", " VEGAN", "KETO"],
+  name: "Classic bacon and eggs",
+  description:
+    "Among other pork products, bacon is high in fat, making it the ideal candidate for a keto-friendly, low carb breakfast",
+  Calories: 410,
+  NutriScore: 80,
+  Rating: 4.8,
+};
+const LVK2 = {
+  img: "img/custom_meals/Salmon.jpg",
+  categories: ["LACTOSE-Free", " VEGAN", "KETO"],
+  name: "Salmon with olive-pistachio tapenade and tomatoes",
+  description:
+    "A foolproof trio loaded with flavor: flavorful salmon dressed up with a juicy olive and crunchy pistachio tapenade",
+  Calories: 450,
+  NutriScore: 80,
+  Rating: 4.9,
+};
+const LVK3 = {
+  img: "img/custom_meals/Zucchini_salad.jpg",
+  categories: ["LACTOSE-Free", " VEGAN", "KETO"],
+  name: "Zucchini salad",
+  description:
+    "Zucchini stands in for potatoes in this low-carb makeover of the classic summer fave, potato salad. All of the creamy flavor, and (almost) none of the carbs! Perfect for picnics and barbecues",
+  Calories: 360,
+  NutriScore: 60,
+  Rating: 4.8,
+};
+const LVK4 = {
+  img: "img/custom_meals/Low_carb_banana_waffles.jpg",
+  categories: ["LACTOSE-Free", " VEGAN", "KETO"],
+  name: "Low carb banana waffles",
+  description:
+    "Love waffles? Adore bananas? Waffles + bananas = perfection! Why not try these delicious, dairy-free waffles for dessert",
+  Calories: 350,
+  NutriScore: 50,
+  Rating: 4.8,
+};
+const VK = {
+  img: "img/custom_meals/Rainbow.jpg",
+  categories: ["VEGETARIAN", "KID-FRIENDLY"],
+  name: "Rainbow veggie kebabs",
+  description:
+    "A fun snack for kids that'll really get 'em eating more veggies! Pair with your fave vegan dip for a super-nutritious rainbow at snack time!",
+  Calories: 300,
+  NutriScore: 50,
+  Rating: 4.6,
+};
+const VK1 = {
+  img: "img/custom_meals/Pizza.jpg",
+  categories: ["VEGETARIAN", "KID-FRIENDLY"],
+  name: "Veggie Pita Pizza with pesto",
+  description:
+    "Your kids will love these Vegan Pita Pizza topped with lots of vegetables, sweet caramelized onions, and a pesto even more vegetables. Quick, easy, and gluten-free!",
+  Calories: 400,
+  NutriScore: 70,
+  Rating: 4.8,
+};
+const VK2 = {
+  img: "img/custom_meals/VEGAN_PESTO_PASTA_SALAD.jpg",
+  categories: ["VEGETARIAN", "KID-FRIENDLY"],
+  name: "VEGAN PESTO PASTA SALAD WITH ROASTED VEGGIES",
+  description:
+    "Fun fusilli pasta, creamy vegan pesto, earthy chickpeas, and bright roasted vegetables, combine for a quick, easy, and colorful vegan pesto pasta salad",
+  Calories: 350,
+  NutriScore: 55,
+  Rating: 4.7,
+};
+const VK3 = {
+  img: "img/custom_meals/Vegan_Breakfast_Tacos.jpg",
+  categories: ["VEGETARIAN", "KID-FRIENDLY"],
+  name: "Vegan Breakfast Tacos",
+  description:
+    "Kids love tacos--why not serve them for breakfast, too? These Vegan Breakfast Tacos are a savory take on breakfast with a Mexican-inspired twist. They’re easy to make, gluten-free, and best of all – delicious!",
+  Calories: 330,
+  NutriScore: 50,
+  Rating: 4.7,
+};
+const VK4 = {
+  img: "img/custom_meals/VEGAN_ZUCCHINI_MUFFINS.jpg",
+  categories: ["VEGETARIAN", "KID-FRIENDLY"],
+  name: "VEGAN ZUCCHINI MUFFINS",
+  description:
+    "These vegan zucchini muffins are soft, sweet, and full of healthy ingredients. With no oil or refined sugar in the batter, you can feel good about serving them to your family",
+  Calories: 310,
+  NutriScore: 50,
+  Rating: 4.8,
+};
+
+const PKL = {
+  img: "img/custom_meals/Chicken.jpg",
+  categories: ["PALEO", "KETO", "LOW-FODMAP"],
+  name: "Chicken Caesar Cobb",
+  description:
+    "Grilled chicken, fresh avocado with veggie toppings and creamy caesar dressing",
+  Calories: 410,
+  NutriScore: 93,
+  Rating: 4.9,
+};
+
+const PKL1 = {
+  img: "img/custom_meals/Seasoned_Chicken_Breast.jpg",
+  categories: ["PALEO", "KETO", "LOW-FODMAP"],
+  name: "Seasoned Chicken Breast",
+  description:
+    "This pub-inspired seasoned chicken breast is a fast family hit. Its simple flavour makes it perfect for a salad or sandwich, as well as the star of a heavier meal",
+  Calories: 400,
+  NutriScore: 90,
+  Rating: 4.7,
+};
+
+const PKL2 = {
+  img: "img/custom_meals/Marinated_chicken.jpg",
+  categories: ["PALEO", "KETO", "LOW-FODMAP"],
+  name: "Marinated chicken",
+  description: "Marinated chicken is wonderful to make for busy nights",
+  Calories: 350,
+  NutriScore: 80,
+  Rating: 4.8,
+};
+
+const PKL3 = {
+  img: "img/custom_meals/Chicken_BlackBean.jpg",
+  categories: ["PALEO", "KETO", "LOW-FODMAP"],
+  name: "Chicken and Black Bean Burrito Skillet",
+  description:
+    "One-skillet chicken dish that can be on the table in just 30 minutes, so it is perfect for those busy weeknights when you’re tempted to grab some takeout",
+  Calories: 360,
+  NutriScore: 83,
+  Rating: 4.9,
+};
+
+const PKL4 = {
+  img: "img/custom_meals/Pollo.jpg",
+  categories: ["PALEO", "KETO", "LOW-FODMAP"],
+  name: "Pollo Asado with Black Beans and Rice",
+  description:
+    "This hearty roast chicken dinner is packed with comforting flavors, which is highlighted in its delicious mix of rice and beans. Plus, the entire dish is also done in under an hour! Ingredients",
+  Calories: 380,
+  NutriScore: 87,
+  Rating: 4.6,
+};
